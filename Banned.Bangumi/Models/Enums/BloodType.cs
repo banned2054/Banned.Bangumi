e51c58274d3ca6b377cb172655929749e38a2083
banned2054 <banned2054@163.com>
2026-07-14 12:02:40 +0800

    docs: 补全血型枚举双语文档

--- Banned.Bangumi/Models/Enums/BloodType.cs
+++ Banned.Bangumi/Models/Enums/BloodType.cs
@@ -1,15 +1,20 @@
 namespace Banned.Bangumi.Models.Enums;
 
 /// <summary>
-/// Blood type of a person. A, B, AB, O
+/// 指定人物或角色的血型。<br/>
+/// Specifies a person's or character's blood type.
 /// </summary>
 public enum BloodType
 {
+    /// <summary>A 型。 / Blood type A.</summary>
     A = 1,
 
+    /// <summary>B 型。 / Blood type B.</summary>
     B = 2,
 
+    /// <summary>AB 型。 / Blood type AB.</summary>
     AB = 3,
 
+    /// <summary>O 型。 / Blood type O.</summary>
     O = 4,
 }
